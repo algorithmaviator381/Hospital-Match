0f05d3a603f39f095b62623bf7aae87d2a358751
Aditya Godse <adimail2404@gmail.com>
2023-05-28 13:34:18 +0530

    updated files for new db

--- API/combine_attributes.py
+++ API/combine_attributes.py
@@ -4,22 +4,19 @@ def attributes_array(hospital):
     attribute_vector = np.array(
         [
             hospital.id,
+            hospital.name,   
+            hospital.doctor_speciality,
+            hospital.phone,
             hospital.cost,
             hospital.icu_facilities,
             hospital.years_of_experience,
-            hospital.hospital_size,
-            hospital.waiting_time,
-            hospital.technology_equipment,
-            hospital.emergency_response_time,
             hospital.specialized_units,
             hospital.infection_control_measures,
             hospital.accreditation,
             hospital.surgical_success_rates,
             hospital.patient_satisfaction,
-            hospital.staff_patient_ratio,
-            hospital.avg_length_of_stay,
-            hospital.availability_specialists,
-            hospital.insurance_coverage
+            hospital.insurance_coverage,
+            hospital.beds_availability
         ]
     )
     
